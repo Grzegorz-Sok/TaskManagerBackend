55dbd952d1b106bd849cef3211a2b451f3fe06a3
Grzegorz <grzegorz.sokolowski1997@gmail.com>
2020-05-21 19:22:24 +0200

    Tests - //todo

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO Task (id, priority, description, deadline, type, status, doers) VALUES (0,2, 'moveTubes', ('2020-06-02T14:19:24.83'), null, 'done', null);
-INSERT INTO Task (id, priority, description, deadline, type, status, doers) VALUES (1,1, 'make smb scared',('2020-06-02T14:19:24.83'), null, 'done', null);
+INSERT INTO Task (id, priority, description, deadline, type, status, doers) VALUES (0,2, 'moveTubes', ('2020-06-02T14:19:24.83'), 'helper', 'done', null);
+INSERT INTO Task (id, priority, description, deadline, type, status, doers) VALUES (1,1, 'make smb scared',('2020-06-02T14:19:24.83'), 'screamer', 'done', null);
 INSERT INTO Meeting (id, time, place, participants ) VALUES (0, ('2020-06-02T14:19:24.83'), 'Monster INC', null);
 INSERT INTO Employee (id, name, surname, role ) VALUES (0, 'Mike', 'Wazovsky', 'helper');
 INSERT INTO Employee (id, name, surname, role ) VALUES (1, 'Jake', 'Sullywan', 'screamer');
